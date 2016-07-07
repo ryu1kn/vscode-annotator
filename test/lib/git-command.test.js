9b2ca6a53120c1ae9e8b39a407a25dd47bd90f79
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-07-07 22:47:01 +1000

    Wrote tests for GitCommand class

---
+++ test/lib/git-command.test.js
@@ -0,0 +1,44 @@
+
+const GitCommand = require('../../lib/git-command');
+
+suite('GitCommand', () => {
+
+    suite('#blame', () => {
+
+        test('it executes git blame with a file path', () => {
+            const shellCommandRunner = {run: sinon.stub().returns(Promise.resolve('BLAME'))};
+            const gitCommand = new GitCommand({shellCommandRunner});
+
+            return gitCommand.blame('/PATH/TO/FILE').then(result => {
+                expect(result).to.eql('BLAME');
+                expect(shellCommandRunner.run).to.have.been.calledWith(
+                    'git', ['blame', '--line-porcelain', '--', '/PATH/TO/FILE'], {cwd: '/PATH/TO'}
+                );
+            });
+        });
+
+        test('it executes git blame with a file path and commit hash', () => {
+            const shellCommandRunner = {run: sinon.stub().returns(Promise.resolve('BLAME'))};
+            const gitCommand = new GitCommand({shellCommandRunner});
+
+            return gitCommand.blame('/PATH/TO/FILE', 'COMMIT').then(result => {
+                expect(result).to.eql('BLAME');
+                expect(shellCommandRunner.run).to.have.been.calledWith(
+                    'git', ['blame', '--line-porcelain', 'COMMIT', '--', '/PATH/TO/FILE'], {cwd: '/PATH/TO'}
+                );
+            });
+        });
+    });
+
+    suite('#getRepositoryRoot', () => {
+
+        test('it executes git rev-parse to get the absolute path of git repository', () => {
+            const shellCommandRunner = {run: sinon.spy()};
+            const gitCommand = new GitCommand({shellCommandRunner});
+            gitCommand.getRepositoryRoot('/PATH/TO/FILE');
+            expect(shellCommandRunner.run).to.have.been.calledWith(
+                'git', ['rev-parse', '--show-toplevel'], {cwd: '/PATH/TO'}
+            );
+        });
+    });
+});
