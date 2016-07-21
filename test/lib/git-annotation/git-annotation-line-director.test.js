b8423b462b2be47402644d1e844048a244c119af
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-07-21 23:39:44 +1000

    Added tests

--- test/lib/git-annotation/git-annotation-line-director.test.js
+++ test/lib/git-annotation/git-annotation-line-director.test.js
@@ -17,7 +17,8 @@ suite('GitAnnotationLineDirector', () => {
                 commitHash: 'COMMIT_HASH',
                 authorName: 'AUTHOR_NAME',
                 authorTime: 1465725065,
-                subject: 'SUBJECT'
+                subject: 'SUBJECT',
+                lineContents: 'LINE_CONTENTS'
             },
             lineNumber: 1,
             lineNumberWidth: 3,
@@ -25,5 +26,14 @@ suite('GitAnnotationLineDirector', () => {
         };
         expect(director.construct(params)).to.be.eql('LINE_HTML');
         expect(builder.addDetails).to.have.been.calledWith('Commit: COMMIT_HASH\nAuthor: AUTHOR_NAME\nDate: 2016-06-12 19:51:05\n\nSUBJECT');
+        expect(builder.addCaption).to.have.been.calledWith('  1  COMMIT_ 2016-06-12 AUTHOR_NAME');
+        expect(builder.addCommand).to.have.been.calledWith({
+            name: 'annotator.takeDiff',
+            args: [{
+                commitHash: 'COMMIT_HASH',
+                repositoryRoot: 'REPOSITORY_ROOT'
+            }]
+        });
+        expect(builder.addLineContents).to.have.been.calledWith('LINE_CONTENTS');
     });
 });
