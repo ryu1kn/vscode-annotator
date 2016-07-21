9e7522f6ed929bd010e61a5c12c1ab9d3567246d
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-07-21 23:13:16 +1000

    Introduced directorFactory to intercept document building

--- test/lib/git-annotation-document-builder.test.js
+++ test/lib/git-annotation-document-builder.test.js
@@ -5,7 +5,11 @@ suite('GitAnnotationDocumentBuilder', () => {
 
     test('it composes annotation HTML document from given annotation data', () => {
         const annotationStyleBuilder = {build: () => 'CSS'};
-        const builder = new GitAnnotationDocumentBuilder({annotationStyleBuilder});
+        const gitAnnotationLineDirector = {construct: sinon.stub().returns('LINE_HTML')};
+        const gitAnnotationLineDirectorFactory = {create: sinon.stub().returns(gitAnnotationLineDirector)};
+        const builder = new GitAnnotationDocumentBuilder({
+            annotationStyleBuilder, gitAnnotationLineDirectorFactory
+        });
         const lines = [{
             filename: 'FILENAME',
             commitHash: 'COMMIT_HASH',
@@ -15,20 +19,19 @@ suite('GitAnnotationDocumentBuilder', () => {
             subject: 'SUBJECT'
         }];
 
-        expect(builder.build(lines, 'REPOSITORY_ROOT')).to.eql([
-            /* eslint-disable indent */
-            '<style>CSS</style>',
-            '<body>',
-                '<div class="line">',
-                    '<div class="annotation" data-details="Commit: COMMIT_HASH&#xa;Author: AUTHOR_NAME&#xa;Date: 2016-06-12 19:51:05&#xa;&#xa;SUBJECT">',
-                        '<a href="command:annotator.takeDiff?%5B%7B%22repositoryRoot%22:%22REPOSITORY_ROOT%22,%22path%22:%22FILENAME%22,%22commitHash%22:%22COMMIT_HASH%22%7D%5D" class="annotation-inner">',
-                            '1  COMMIT_ 2016-06-12 AUTHOR_NAME',
-                        '</a>',
-                    '</div>',
-                    '<pre><code>LINE_CONTENTS</code></pre>',
-                '</div>',
-            '</body>'
-            /* eslint-enable indent */
-        ].join(''));
+        expect(builder.build(lines, 'REPOSITORY_ROOT')).to.eql('<style>CSS</style><body>LINE_HTML</body>');
+        expect(gitAnnotationLineDirector.construct).to.have.been.calledWith({
+            lineBlame: {
+                authorName: 'AUTHOR_NAME',
+                authorTime: 1465725065,
+                commitHash: 'COMMIT_HASH',
+                filename: 'FILENAME',
+                lineContents: 'LINE_CONTENTS',
+                subject: 'SUBJECT'
+            },
+            lineNumber: 1,
+            lineNumberWidth: 1,
+            repositoryRoot: 'REPOSITORY_ROOT'
+        });
     });
 });
