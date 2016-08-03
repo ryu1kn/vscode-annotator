e71287debfbf2a49554d98f4cef9eaad5850732a
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-08-03 23:20:57 +1000

    refs #6 Use git.path value if it's available

--- test/lib/config-store.test.js
+++ test/lib/config-store.test.js
@@ -3,14 +3,21 @@ const ConfigStore = require('../../lib/config-store');
 
 suite('ConfigStore', () => {
 
-    test('it gets the current editor config from vscode.workspace', () => {
-        const extensionConfig = {get: stubWithArgs(['CONFIG_NAME'], 'CONFIG_VALUE')};
-        const workspace = {getConfiguration: stubWithArgs(['editor'], extensionConfig)};
+    test('it reads the current editor config from vscode.workspace', () => {
+        const editorConfig = {get: stubWithArgs(['CONFIG_NAME'], 'CONFIG_VALUE')};
+        const workspace = {getConfiguration: stubWithArgs(['editor'], editorConfig)};
         const configStore = new ConfigStore({workspace});
         expect(configStore.getEditorConfig('CONFIG_NAME')).to.eql('CONFIG_VALUE');
     });
 
-    test('it gets the current extension config from vscode.workspace', () => {
+    test('it reads the current editor config from vscode.workspace', () => {
+        const gitConfig = {get: stubWithArgs(['CONFIG_NAME'], 'CONFIG_VALUE')};
+        const workspace = {getConfiguration: stubWithArgs(['git'], gitConfig)};
+        const configStore = new ConfigStore({workspace});
+        expect(configStore.getGitConfig('CONFIG_NAME')).to.eql('CONFIG_VALUE');
+    });
+
+    test('it reads the current git config from vscode.workspace', () => {
         const extensionConfig = {get: stubWithArgs(['CONFIG_NAME'], 'CONFIG_VALUE')};
         const workspace = {getConfiguration: stubWithArgs(['annotator'], extensionConfig)};
         const configStore = new ConfigStore({workspace});
--- test/lib/git-service.test.js
+++ test/lib/git-service.test.js
@@ -6,42 +6,64 @@ suite('GitService', () => {
     suite('#gitBlame', () => {
 
         test('it executes git blame with a file path and commit hash', () => {
+            const configStore = {getGitConfig: () => '/PATH/TO/GIT'};
             const shellCommandRunner = {run: sinon.stub().returns(Promise.resolve('BLAME'))};
             const gitBlameOutputParser = {parse: stubWithArgs(['BLAME'], 'PARSED_BLAME')};
-            const gitService = new GitService({gitBlameOutputParser, shellCommandRunner});
+            const gitService = new GitService({configStore, gitBlameOutputParser, shellCommandRunner});
 
             return gitService.getBlame('/PATH/TO/FILE', 'COMMIT_HASH', 'ROOT_PATH').then(result => {
                 expect(result).to.eql('PARSED_BLAME');
                 expect(shellCommandRunner.run).to.have.been.calledWith(
-                    'git', ['blame', '--line-porcelain', 'COMMIT_HASH', '--', '/PATH/TO/FILE'], {cwd: 'ROOT_PATH'}
+                    '/PATH/TO/GIT',
+                    ['blame', '--line-porcelain', 'COMMIT_HASH', '--', '/PATH/TO/FILE'],
+                    {cwd: 'ROOT_PATH'}
                 );
             });
         });
 
         test('it invokes git blame without commitHash if it is not given', () => {
+            const configStore = {getGitConfig: () => '/PATH/TO/GIT'};
             const shellCommandRunner = {run: sinon.stub().returns(Promise.resolve('BLAME'))};
             const gitBlameOutputParser = {parse: stubWithArgs(['BLAME'], 'PARSED_BLAME')};
-            const gitService = new GitService({gitBlameOutputParser, shellCommandRunner});
+            const gitService = new GitService({configStore, gitBlameOutputParser, shellCommandRunner});
 
             const commitHash = undefined;
             return gitService.getBlame('/PATH/TO/FILE', commitHash, 'ROOT_PATH').then(result => {
                 expect(result).to.eql('PARSED_BLAME');
                 expect(shellCommandRunner.run).to.have.been.calledWith(
-                    'git', ['blame', '--line-porcelain', '--', '/PATH/TO/FILE'], {cwd: 'ROOT_PATH'}
+                    '/PATH/TO/GIT', ['blame', '--line-porcelain', '--', '/PATH/TO/FILE'], {cwd: 'ROOT_PATH'}
                 );
             });
+        });
+    });
+
+    suite('#_git', () => {
+
+        test('it holds the git path specified in the settings', () => {
+            const configStore = {getGitConfig: stubWithArgs(['path'], '/PATH/TO/GIT')};
+            const gitService = new GitService({configStore});
+            expect(gitService._gitPath).to.eql('/PATH/TO/GIT');
+        });
+
+        test('it returns "git" if the git path is not specified in the settings', () => {
+            const configStore = {getGitConfig: () => null};
+            const gitService = new GitService({configStore});
+            expect(gitService._gitPath).to.eql('git');
         });
     });
 
     suite('#getChangedFilesInCommit', () => {
 
         test('it lists all the changed files in a commit with their change type (e.g. M/A/D/...)', () => {
+            const configStore = {getGitConfig: () => '/PATH/TO/GIT'};
             const changedFileListParser = {parse: sinon.spy()};
             const shellCommandRunner = {run: sinon.stub().returns(Promise.resolve('GIT_OUTPUT'))};
-            const gitService = new GitService({changedFileListParser, shellCommandRunner});
+            const gitService = new GitService({configStore, changedFileListParser, shellCommandRunner});
             return gitService.getChangedFilesInCommit('COMMIT_HASH', 'REPOSITORY_ROOT').then(() => {
                 expect(shellCommandRunner.run).to.have.been.calledWith(
-                    'git', ['diff-tree', 'COMMIT_HASH', '--name-status', '--parents', '--root', '-M', '-r'], {cwd: 'REPOSITORY_ROOT'}
+                    '/PATH/TO/GIT',
+                    ['diff-tree', 'COMMIT_HASH', '--name-status', '--parents', '--root', '-M', '-r'],
+                    {cwd: 'REPOSITORY_ROOT'}
                 );
                 expect(changedFileListParser.parse).to.have.been.calledWith('GIT_OUTPUT');
             });
@@ -51,12 +73,13 @@ suite('GitService', () => {
     suite('#getRepositoryRoot', () => {
 
         test('it executes git rev-parse to get the absolute path of git repository', () => {
+            const configStore = {getGitConfig: () => '/PATH/TO/GIT'};
             const shellCommandRunner = {run: sinon.stub().returns(Promise.resolve('PATH\n'))};
-            const gitService = new GitService({shellCommandRunner});
+            const gitService = new GitService({configStore, shellCommandRunner});
             return gitService.getRepositoryRoot('/PATH/TO/FILE').then(path => {
                 expect(path).to.eql('PATH');
                 expect(shellCommandRunner.run).to.have.been.calledWith(
-                    'git', ['rev-parse', '--show-toplevel'], {cwd: '/PATH/TO'}
+                    '/PATH/TO/GIT', ['rev-parse', '--show-toplevel'], {cwd: '/PATH/TO'}
                 );
             });
         });
@@ -65,11 +88,12 @@ suite('GitService', () => {
     suite('#getFileContents', () => {
 
         test('it shows the contents of a specified file of a specified commit', () => {
+            const configStore = {getGitConfig: () => '/PATH/TO/GIT'};
             const shellCommandRunner = {run: sinon.spy()};
-            const gitService = new GitService({shellCommandRunner});
+            const gitService = new GitService({configStore, shellCommandRunner});
             gitService.getFileContents('COMMIT', '/PATH/TO/FILE', 'ROOT_PATH');
             expect(shellCommandRunner.run).to.have.been.calledWith(
-                'git', ['show', 'COMMIT:/PATH/TO/FILE'], {cwd: 'ROOT_PATH'}
+                '/PATH/TO/GIT', ['show', 'COMMIT:/PATH/TO/FILE'], {cwd: 'ROOT_PATH'}
             );
         });
     });
