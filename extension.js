57aa4c84e97acecd9d4fc8304d5d03e0f06d58e0
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-04-25 16:09:46 +1000

    [#18] Give extension context to the annotate command

--- extension.js
+++ extension.js
@@ -3,7 +3,7 @@
 const AppIntegratorFactory = require('./lib/app-integrator-factory');
 
 exports.activate = context => {
-    const appIntegrator = new AppIntegratorFactory().create();
+    const appIntegrator = new AppIntegratorFactory(context).create();
     appIntegrator.integrate(context);
 };
 
